f63368ff2e2538b81138c6aa1fb035d261929b9f
PrateekMDesai04 <prateekmdesai04@gmail.com>
2021-04-21 23:06:08 +0400

    Hashed Password

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const UserModel = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
   // VALIDATE
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+
+  // Check if email is present
+  const emailExist = await UserModel.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send('E-mail already exists !');
+
+  // Hash the password once validation done
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt); //HASHING PASSWORD
+
+  //Save to DB
   const user = new UserModel({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   try {
